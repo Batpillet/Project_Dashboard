4b3d4dfbb87b80e74e31e311ebd15bbe5343aa08
TreyPrase <batpillet@gmail.com>
2019-12-10 17:12:31 +0100

    Tableau dynamique dernier widget

--- projet/src/components/TableWidget.js
+++ projet/src/components/TableWidget.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react'
 
 export default class TableWidget extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            sensors: [
+                { id: 0, date: "09/12/19", userId: 2, location: "salon" },
+                { id: 1, date: "09/07/19", userId: 2, location: "chambre" }
+            ]
+        }
+    }
+
+    renderTableData() {
+        return this.state.sensors.map((sensor, index) => {
+            const { id, date, userId, location } = sensor
+            return (
+                <tr key={id}>
+                    <td>{id}</td>
+                    <td>{date}</td>
+                    <td>{userId}</td>
+                    <td>{location}</td>
+                </tr>
+            )
+        })
+    }
+
+    renderTableHeader() {
+        let header = Object.keys(this.state.sensors[0])
+        return header.map((key, index) => {
+            return <th key={index}>{key.toUpperCase()}</th>
+        })
+    }
+
     render() {
         return (
             <div className="card shadow">
@@ -9,28 +40,11 @@ export default class TableWidget extends Component {
                         <table class="table table-striped table-sm">
                             <thead>
                                 <tr>
-                                    <th>#ID</th>
-                                    <th>Nom</th>
-                                    <th>Prenom</th>
-                                    <th>Objet</th>
-                                    <th>Pays</th>
+                                    {this.renderTableHeader()}
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>0</td>
-                                    <td>PILLET</td>
-                                    <td>Baptiste</td>
-                                    <td>Montre</td>
-                                    <td>France</td>
-                                </tr>
-                                <tr>
-                                    <td>1</td>
-                                    <td>JOUSSELIN</td>
-                                    <td>Gaultier</td>
-                                    <td>OnePlus</td>
-                                    <td>France</td>
-                                </tr>
+                                {this.renderTableData()}
                             </tbody>
                         </table>
                     </div>
